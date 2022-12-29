a5264cd0c08c1dc57fd78eb4a15bf764126c8b02
AlomgirH <102249051+Alomgir01@users.noreply.github.com>
2022-12-29 20:39:10 +0600

    Add files via upload

---
+++ Ex9.java
@@ -0,0 +1,16 @@
+public class Ex9
+{
+    public static int square(int num) 
+    {
+        return num*num;
+    }
+    public static void main(String[]args)
+    {
+        int result= square(10);
+    
+        System.out.print("Square of this number is: "+result);
+    }
+
+}
+
+                                                                            
